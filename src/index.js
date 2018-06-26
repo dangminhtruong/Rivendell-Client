2b670d09ae72c148add9d10dd1cf52c0592263e8
dangminhtruong <xdangminhtruongx@gmail.com>
2018-06-26 21:27:50 +0700

    structure

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    //burgerBuilder: burgerBuilderReducer,
+    //order: orderReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
